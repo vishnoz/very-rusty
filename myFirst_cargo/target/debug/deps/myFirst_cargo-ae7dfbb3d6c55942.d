be23957929e19025167fd688f72846ec1ad3d99b
Nozomi Rajan <nozomirajan@nozomis-air.speedport.ip>
2024-03-24 20:14:36 +0100

    my First rust program
    !
    !

---
+++ myFirst_cargo/target/debug/deps/myFirst_cargo-ae7dfbb3d6c55942.d
@@ -0,0 +1,5 @@
+/Users/nozomirajan/projects/git/very-rusty/very-rusty-1/myFirst_cargo/target/debug/deps/myFirst_cargo-ae7dfbb3d6c55942: src/main.rs
+
+/Users/nozomirajan/projects/git/very-rusty/very-rusty-1/myFirst_cargo/target/debug/deps/myFirst_cargo-ae7dfbb3d6c55942.d: src/main.rs
+
+src/main.rs:
---
+++ myFirst_cargo/target/debug/myFirst_cargo.d
@@ -0,0 +1,1 @@
+/Users/nozomirajan/projects/git/very-rusty/very-rusty-1/myFirst_cargo/target/debug/myFirst_cargo: /Users/nozomirajan/projects/git/very-rusty/very-rusty-1/myFirst_cargo/src/main.rs
